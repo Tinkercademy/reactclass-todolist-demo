eed245082949d967e47b76a742c3c5df769e934d
yuhaochua <yuhaochua99@gmail.com>
2020-04-14 17:14:21 +0800

    Rendering todo list and delete button

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component{
     this.handleTask = this.handleTask.bind(this);
     this.handleDueDate = this.handleDueDate.bind(this);
     this.addTask = this.addTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
   }
 
   handleTask(e){
@@ -47,6 +48,36 @@ class App extends Component{
     })
   }
 
+  deleteTask(taskToDelete){
+    let updatedToDoList = []
+    this.state.toDoList.forEach(task =>{
+      if(task.ID !== taskToDelete.ID){
+        updatedToDoList = [...updatedToDoList, task]
+      }
+    })
+    this.setState({
+      toDoList: updatedToDoList
+    })
+  }
+
+  renderToDo(){
+    return this.state.toDoList.map(task => {
+      return(
+        <div className="container task mb-2 p-2" key={task.ID}>
+            <div className="row">
+              <div className="col-9">
+                  <div className="dueDate pb-1">Due: {task.date}</div>
+                  <span>{task.description}</span>
+              </div>
+              <div className="col-3 pt-2">
+                <button className="btn btn-danger" onClick = {() => this.deleteTask(task)}>X</button>
+              </div>
+            </div>
+          </div>
+      );
+    })
+  }
+
   render(){
     return (
       <div className="App pt-5">
@@ -56,6 +87,16 @@ class App extends Component{
             <input className="taskSetter" onChange = {this.handleTask} type="text" placeholder="Add task here..."/>
             <input onChange = {this.handleDueDate} type="date" />
             <button className="btn btn-primary ml-3" onClick = {() => this.addTask()}>Add!</button>
+          </div>
+
+          <div className="container">
+            <div className="row">
+              <div className="col-3"></div>
+              <div className="col-6">
+                {this.renderToDo() }
+              </div>
+              <div className="col-3"></div>
+            </div>
           </div>
       </div>
     );
